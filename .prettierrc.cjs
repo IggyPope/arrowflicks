318c8c6fcff838e739665b878675ea5b3c2dbaed
IggyPope <altko4egar@gmail.com>
2024-05-16 15:15:50 +0300

    chore: add import sorting to prettier and css props ordering to stylelint

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,1 +1,14 @@
-module.exports = require('eslint-config-mantine/.prettierrc.js');
+const mantineConfig = require('eslint-config-mantine/.prettierrc.js');
+
+module.exports = {
+  ...mantineConfig,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'lf',
+  importOrder: ['^react(.*)', 'mantine/(.*)', '<THIRD_PARTY_MODULES>', '@/(.*)', '^[./]'],
+  importOrderSeparation: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
